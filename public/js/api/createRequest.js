b8564f76891b6078f022eabdc7b156b2383932be
solarlime <solarlime@outlook.com>
2019-12-11 12:21:02 +0300

    Small fixes

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -8,8 +8,12 @@ const createRequest = (options = {}) => {
     let connection = new XMLHttpRequest();
     let formData = new FormData;
     let url = options.url;
+
+    connection.responseType = options.responseType;
+    connection.withCredentials = true;
+
     if (options.headers !== undefined) {
-        console.log(Object.keys(options.headers)[0], Object.values(options.headers)[0]);
+        connection.setRequestHeader(Object.keys(options.headers)[0].toString(), Object.values(options.headers)[0].toString());
     }
     if (options.method === 'GET') {
         url = `${url}?`;
@@ -18,33 +22,25 @@ const createRequest = (options = {}) => {
         }
         url = url.slice(0, url.length - 1);
     }
-    connection.open(options.method, url);
-    connection.responseType = options.responseType;
-    connection.withCredentials = true;
-    if (options.headers !== undefined) {
-        connection.setRequestHeader(Object.keys(options.headers)[0].toString(), Object.values(options.headers)[0].toString());
-    }
-    if (options.method === 'GET') {
-        connection.send();
-    }
     else {
         for (let item in options.data) {
             formData.append(item, options.data[item]);
         }
+    }
+    connection.open(options.method, url);
+    if (options.method === 'GET') {
+        connection.send();
+    }
+    else {
         connection.send(formData);
     }
 
-    connection.onload = function () {
-        if (connection.status !== 200) {
-            options.callback(connection.status, 0);
+    connection.onreadystatechange = function () {
+        try {
+            options.callback(0, connection.response);
         }
-        else {
-            try {
-                options.callback(0, connection.response);
-            }
-            catch (e) {
-                options.callback(e, 0);
-            }
+        catch (e) {
+            options.callback(e, 0);
         }
     };
     return connection;
