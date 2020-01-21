44a2a8edc8576e955f0857ceb2a8d95fa3ade678
Solarlime <solarlime@outlook.com>
2020-01-21 18:58:07 +0300

    First version

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -3,7 +3,7 @@
  * регистрации
  * Наследуется от AsyncForm
  * */
-class RegisterForm {
+class RegisterForm extends AsyncForm {
   /**
    * Производит регистрацию с помощью User.register
    * После успешной регистрации устанавливает
@@ -11,5 +11,12 @@ class RegisterForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(options) {
+    User.register(options, (err, response) => {
+      if (response) {
+        App.setState('user-logged');
+        const close = new Modal(document.querySelector('#modal-register'));
+        close.close();
+      }
+    });
   }
 }
