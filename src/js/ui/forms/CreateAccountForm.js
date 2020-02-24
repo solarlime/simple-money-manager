468df2f87aa829a07b5f0692bba77d8951c7e017
Solarlime <solarlime@outlook.com>
2020-02-24 03:54:32 +0300

    Added a confirmation for an existing account name

--- src/js/ui/forms/CreateAccountForm.js
+++ src/js/ui/forms/CreateAccountForm.js
@@ -13,15 +13,27 @@ export default class CreateAccountForm extends AsyncForm {
    * Создаёт счёт с помощью Account.create и закрывает
    * окно (в котором находится форма) в случае успеха,
    * а также вызывает App.update()
-   * и сбрасывает форму
+   * и сбрасывает форму.
+   * Предварительно проверяет, использовано ли уже
+   * такое имя и запрашивает подтверждение.
    * */
   onSubmit(options, callback) {
-    Account.create(options, (err, response) => {
-      if (response) {
-        App.getModal('createAccount').close();
-        callback();
-        App.update();
-      }
-    });
+    const duplicate = Array.from(document.querySelectorAll('li.account'))
+      .map((item) => item.innerText.split(' / ')[0])
+      .find((item) => item === options.name);
+    let confirmation = true;
+    if (duplicate) {
+      // eslint-disable-next-line no-restricted-globals
+      confirmation = confirm('Такое имя уже использовано. Всё равно создать?');
+    }
+    if (confirmation) {
+      Account.create(options, (err, response) => {
+        if (response) {
+          App.getModal('createAccount').close();
+          callback();
+          App.update();
+        }
+      });
+    }
   }
 }
